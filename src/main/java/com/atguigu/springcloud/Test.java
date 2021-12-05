bbca29953fc34b3b2d777028d13946c9bf5fbde8
suj <3212174398@qq.com>
2021-12-05 21:14:33 +0800

    hot_fix commit 2

--- src/main/java/com/atguigu/springcloud/Test.java
+++ src/main/java/com/atguigu/springcloud/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello world!!!");
         System.out.println("hot_fix");
+        System.out.println("hello hot_fix");
     }
 }
